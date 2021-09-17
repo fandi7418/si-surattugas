1b31d007c0e8f6db376c5fed0f7fbca51d393fcd
claten <38368245+claten@users.noreply.github.com>
2021-09-17 17:02:45 +0700

    tambah-tambah

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
     public function index()
     {
-        return view('login',[
-            'title' => 'Halaman Login'
+        $request = DB::table('users')->get();
+        return view('login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required',
         ]);
-    
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboarddosen');
+        }
+
+        return back()->with('loginError', 'Login Gagal!');
     }
 }
-
