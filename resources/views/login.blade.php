8778bc67b3d7597014a6dcf3a281d304b99d0c24
fandi7418 <mgesitalifandi@gmail.com>
2021-10-06 11:35:20 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -11,66 +11,74 @@
 
     <link rel="canonical" href="{{ asset('https://getbootstrap.com/docs/5.1/examples/sign-in/') }}">
 
-    
+
 
     <!-- Bootstrap core CSS -->
-    <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css') }}" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-      }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
     </style>
 
-    
+
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
-  </head>
-    <body class="text-center">
-  
+</head>
+
+<body class="text-center">
 
 
 
-<main class="form-signin">
-  <form action="{{ route('postlogin') }}" method="post">
-    @csrf
-    {{-- @if(session()->has('loginError')) --}}
-    {{-- <div class="alert alert-danger alert-dismissible fade show" role="alert"> --}}
-      {{-- {{ session('loginError') }} --}}
-      {{-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-lable="Close"></button>
+
+    <main class="form-signin">
+      
+        <form action="{{ route('postlogin') }}" method="post">
+            @csrf
+            {{-- @if(session()->has('loginError')) --}}
+            {{-- <div class="alert alert-danger alert-dismissible fade show" role="alert"> --}}
+            {{-- {{ session('loginError') }} --}}
+            {{-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-lable="Close"></button>
     </div> --}}
-    {{-- @endif --}}
-    <img class="mb-4" src="/undip.png" alt="" width="auto" height="150">
-    <h1 class="h3 mb-3 fw-normal">Silahkan Login</h1>
+            {{-- @endif --}}
+            <img class="mb-4" src="/undip.png" alt="" width="auto" height="150">
+            <h1 class="h3 mb-3 fw-normal">Silahkan Login</h1>
 
-    <div class="form-floating">
-      <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" autofocus required>
-      <label for="email">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-      <label for="password">Password</label>
-    </div>
-    <div>
-      <button class="btn btn-lg btn-primary" type="submit">Login</button>
-    </div>
-  </form>
-</main>
+            <div class="form-floating">
+                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com"
+                    autofocus required>
+                <label for="email">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" name="password" class="form-control" id="password" placeholder="Password"
+                    required>
+                <label for="password">Password</label>
+            </div>
+            <div>
+                <button class="btn btn-lg btn-primary" type="submit">Login</button>
+            </div>
+        </form>
+    </main>
 
 
-    
-  </body>
+
+</body>
+
 </html>
 @include('sweetalert::alert')
-<script src="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js') }}" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<script src="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js') }}"
+    integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+</script>
