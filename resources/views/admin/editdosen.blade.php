5f6cc5d7cd837307f0e386bdb2b1ab539ef51344
fandi7418 <mgesitalifandi@gmail.com>
2021-10-25 20:29:44 +0700

    tambah validate update admin dosenssss

--- resources/views/admin/editdosen.blade.php
+++ resources/views/admin/editdosen.blade.php
@@ -97,7 +97,7 @@
             </div>
             </div>
             @endforeach
-            <!-- Form Pop Up Reset Password -->
+            <!-- Form Pop Up Reset Password  test-->
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                     <div class="modal-content">
