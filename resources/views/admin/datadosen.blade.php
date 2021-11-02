e42267ace47091b235fbf119c312047cac019b2f
fandi7418 <mgesitalifandi@gmail.com>
2021-11-02 19:03:59 +0700

    edit data admin

--- resources/views/admin/datadosen.blade.php
+++ resources/views/admin/datadosen.blade.php
@@ -1,87 +1,131 @@
 @extends('admin.main')
 
 @section('container')
-<h2 class="mt-4">Data Dosen</h2>
-
-      <div class="table-responsive">
-          <select class="btn btn-secondary dropdown-toggle btn-sm" href="#" id="filter" data-bs-toggle="dropdown" aria-expanded="false" style="float: right">Pilih Program Studi
-            <ul class="dropdown-menu" aria-labelledby="dropdown03">
-              <li><option selected class="dropdown-item-dark" href="">Semua</option></li>
-              <li><option value="" class="dropdown-item-dark" href="#"></option></li>
-              @foreach ($prodi as $prodis )
-              <option value="{{ $prodis->id }}" {{ old('prodi_id') == $prodis->id ? 'selected' : null }}>{{ $prodis->prodi }}</option>
-              @endforeach
-            </ul>
-          </select>
-          <!-- {{-- <form action=""></form> --}} -->
+<br>
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Data Dosen</h3>
+    </div>
+    <div class="card-body">
         <a href="/tambah_dosen" class="">
-        <button type="submit"  class="btn btn-success btn-sm" style="float: right; margin-right: 5px">Tambah Dosen</button>
-      </a>
-      <br>
-      <br>
-        <table class="table table-striped table-bordered table-sm" id="datadosen">
-          <thead>
+            <button type="button" class="btn btn-success btn-sm">
+                Tambah Dosen</button>
+        </a>
+        {{-- <select class="btn btn-secondary btn-sm filter" style="float: right" href="#" id="filter-prodis"
+            aria-expanded="false" style="">Pilih Program Studi
+            <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                <li>
+                    <option selected class="dropdown-item-dark" href="">Semua</option>
+                </li>
+                @foreach ($prodi as $prodis )
+                <option value="{{ $prodis->id }}">
+                    {{ $prodis->prodi }}</option>
+                @endforeach
+            </ul>
+        </select> --}}
+        <br>
+        <br>
+        <table class="table table-striped table-bordered table-sm" style="width:100%" id="datadosen">
+            <thead>
 
-<div class="table-responsive">
-    <select class="btn btn-secondary dropdown-toggle btn-sm" href="#" id="filter" data-bs-toggle="dropdown"
-        aria-expanded="false" style="float: right">Pilih Program Studi
-        <ul class="dropdown-menu" aria-labelledby="dropdown03">
-            <li>
-                <option selected class="dropdown-item-dark" href="">Semua</option>
-            </li>
-            @foreach ($prodi as $prodis )
-            <option value="{{ $prodis->id }}" {{ old('prodi_id') == $prodis->id ? 'selected' : null }}>
-                {{ $prodis->prodi }}</option>
-            @endforeach
-        </ul>
-    </select>
+                <tr>
+                    <th scope="col">Nama Dosen</th>
+                    <th scope="col">NIP</th>
+                    <th scope="col">Program Studi</th>
+                    <th scope="col">Aksi</th>
+                </tr>
 
-    <a href="/tambah_dosen" class="">
-        <button type="submit" class="btn btn-success btn-sm" style="float: right; margin-right: 5px">
-          Tambah Dosen</button>
-    </a>
-    <br>
-    <br>
-    <table class="table table-striped table-bordered table-sm" style="width:100%" id="datadosen">
-        <thead>
-
-            <tr>
-                <th scope="col">Nama Dosen</th>
-                <th scope="col">NIP</th>
-                <th scope="col">Program Studi</th>
-                <th scope="col">Aksi</th>
-            </tr>
-
-          </thead>
+            </thead>
         </table>
-      </div>
-    
-    @endsection
-    @push('scripts')
-        <script>
-              $(document).ready(function() {
-                  $('#datadosen').DataTable({
-                      processing : true,
-                      serverside : true,
-                      ajax : {
-                        url: "{{ route('data dosen') }}",
-                        type: 'GET'
-                      },
-                      columns:[
-                        {data:'nama_dosen', name:'nama_dosen'},
-                        {data:'NIP', name:'NIP'},
-                        {data:'prodi.prodi', name:'prodi.prodi'},
-                        {
-                            data: 'action', 
-                            name: 'action', 
-                            orderable: true, 
-                            searchable: true
-                        },
-                      ],
-                      order: [[0,'asc']]
-                  });
-              } );
-          </script>
-      @endpush
+    </div>
+</div>
 
-       
+
+@endsection
+@push('scripts')
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#datadosen').DataTable({
+            processing: true,
+            serverside: true,
+            ajax: {
+                url: "{{ route('data dosen') }}",
+                type: 'GET'
+            },
+            columns: [{
+                    data: 'nama_dosen',
+                    name: 'nama_dosen'
+                },
+                {
+                    data: 'NIP',
+                    name: 'NIP'
+                },
+                {
+                    data: 'prodi.prodi',
+                    name: 'prodi.prodi'
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: false,
+                    searchable: false
+                },
+            ],
+            order: [
+                [0, 'asc']
+            ]
+        });
+    });
+    $(".filter").on('change', function () {
+        prodis = $("#filter-prodis").val()
+        ajax.reload(null, false)
+
+    })
+    // $(document).ready(function () {
+    //     fetch_data();
+    //     function fetch_data(prodi = '')
+    //     {
+    //     $('#datadosen').DataTable({
+    //         "oLanguage" : {
+    //             "sProcessing": "<span>Memuat Data...</span>"  
+    //         },
+    //         serverside: true,
+    //         processing: true,
+    //         ajax: {
+    //             url: "{{ route('data dosen') }}",
+    //             data: {prodi:prodi}
+    //         },
+    //         columns: [{
+    //                 data: 'nama_dosen',
+    //                 name: 'nama_dosen'
+    //             },
+    //             {
+    //                 data: 'NIP',
+    //                 name: 'NIP'
+    //             },
+    //             {
+    //                 data: 'prodi',
+    //                 name: 'prodi',
+    //                 orderable: false
+    //             },
+    //             {
+    //                 data: 'action',
+    //                 name: 'action',
+    //             },
+    //         ],
+    //         order: [
+    //             [0, 'asc']
+    //         ]
+    //     });
+    //     }
+    //     $('#filter-prodis').change(function(){
+    //         var id = $('#filter-prodis').val();
+
+    //         $('#datadosen').DataTable().destroy();
+
+    //         fetch_data(id);
+    //     });
+    // });
+
+</script>
+@endpush
--- resources/views/admin/datapetugas.blade.php
+++ resources/views/admin/datapetugas.blade.php
@@ -1,65 +1,65 @@
 @extends('admin.main')
 
 @section('container')
-                    <h2 class="mt-4">Data Petugas Penomoran</h2>
-      <div class="table-responsive">
+<br>
+<div class="card">
+    <div class="card-header">
+        <h2 class="card-title">Data Petugas Penomoran</h2>
+    </div>
+    <div class="card-body">
         <a href="/tambah_petugas" class="">
-                    <button type="button" class="btn btn-success btn-sm" style="float: right; margin-right: 5px">Tambah Petugas</button>
+            <button type="button" class="btn btn-success btn-sm" style="">Tambah
+                Petugas</button>
         </a>
         <br>
         <br>
-          <table class="table table-striped table-bordered table-sm" id="datapetugas" style="width: 100%">
-          <thead>
-            <tr>
-              <th scope="col">Nama Petugas</th>
-              <th scope="col">NIP</th>
-              <th scope="col">E-mail Petugas</th>
-              <th scope="col">Aksi</th>
-            </tr>
-          </thead>
-          {{-- <tbody>
-            @foreach ($petugas as $ptgs )
-              
-            <tr>
-              <td>{{ $ptgs->nama_petugas }}</td>
-              <td>{{ $ptgs->NIP }}</td>
-              <td>{{ $ptgs->email_petugas }}</td>
-              <td>
-                <a href="/edit_petugas/{{ $ptgs->id }}" class="btn btn-primary btn-sm">Edit</a>
-                <a href="/hapus_petugas/{{ $ptgs->id }}/konfirmasi" class="btn btn-danger btn-sm">Hapus</a>
-              </td>
-            </tr>
-            
-            @endforeach
-          </tbody> --}}
+        <table class="table table-striped table-bordered table-sm" id="datapetugas" style="width: 100%">
+            <thead>
+                <tr>
+                    <th scope="col">Nama Petugas</th>
+                    <th scope="col">NIP</th>
+                    <th scope="col">E-mail Petugas</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
         </table>
-      </div>
+    </div>
+</div>
+@endsection
+@push('scripts')
+<script>
+    $(document).ready(function () {
+        $('#datapetugas').DataTable({
+            processing: true,
+            serverside: true,
+            ajax: {
+                url: "{{ route('data petugas') }}",
+                type: 'GET'
+            },
+            columns: [{
+                    data: 'nama_petugas',
+                    name: 'nama_petugas'
+                },
+                {
+                    data: 'NIP',
+                    name: 'NIP'
+                },
+                {
+                    data: 'email_petugas',
+                    name: 'email_petugas'
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: true,
+                    searchable: true
+                },
+            ],
+            order: [
+                [0, 'asc']
+            ]
+        });
+    });
 
-      @endsection
-
-      @push('scripts')
-        <script>
-              $(document).ready(function() {
-                  $('#datapetugas').DataTable({
-                      processing : true,
-                      serverside : true,
-                      ajax : {
-                        url: "{{ route('data petugas') }}",
-                        type: 'GET'
-                      },
-                      columns:[
-                        {data:'nama_petugas', name:'nama_petugas'},
-                        {data:'NIP', name:'NIP'},
-                        {data:'email_petugas', name:'email_petugas'},
-                        {
-                            data: 'action', 
-                            name: 'action', 
-                            orderable: true, 
-                            searchable: true
-                        },
-                      ],
-                      order: [[0,'asc']]
-                  });
-              } );
-          </script>
-      @endpush
+</script>
+@endpush
