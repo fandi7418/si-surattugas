9c7135d0bdcc8ef75c4bcccf6c084bedb7f22cb6
claten <38368245+claten@users.noreply.github.com>
2022-03-16 17:23:52 +0700

    kelupaan hapus seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(AdminSeeder::class);
-        $this->call(UserSeeder::class);
         $this->call(PenggunaSeeder::class);
         $this->call(ProdiSeeder::class);
-        $this->call(KadepSeeder::class);
-        $this->call(PetugasSeeder::class);
-        $this->call(WakilDekanSeeder::class);
         $this->call(SuratSeeder::class);
         $this->call(StatusSuratSeeder::class);
         $this->call(RolesSeeder::class);
